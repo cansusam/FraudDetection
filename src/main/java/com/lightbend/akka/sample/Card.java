db4e2e2bd0d1a3f2e83294ee01db3639d3491d49
cansusam <cansusam@gmail.com>
2018-06-07 11:49:12 +0300

    Basic transaction components

---
+++ src/main/java/com/lightbend/akka/sample/Card.java
@@ -0,0 +1,78 @@
+package com.lightbend.akka.sample;
+
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.Props;
+import com.lightbend.akka.sample.Terminal.receivedAmount;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+
+import java.util.Random;
+
+//#Cart-messages
+public class Card extends AbstractActor {
+    /**
+     * whichTerminal: Transaction request receiver, terminalID
+     * Transaction: instruction to execute transaction
+     * Greeting: message containing the greeting
+     */
+
+
+    // used to catch unknown messages sent to this actor
+    private final LoggingAdapter log = Logging.getLogger(getContext().getSystem(), this);
+
+    private static Integer idCounter = 0;
+
+    //#Cart-messages
+    static public Props props(Integer amount, ActorRef terminalActor) {
+        return Props.create(Card.class, () -> new Card(amount, terminalActor));
+    }
+
+    //#transaction request receiver
+    static public class whichTerminal {
+        public final Integer terminalID;
+
+        public whichTerminal(Integer terminalID) {
+            this.terminalID = terminalID;
+        }
+    }
+
+
+    static public class Transaction {
+        public Transaction() {
+        }
+    }
+    //#Card-messages
+
+    private final Integer amount;
+    private final ActorRef terminalActor;
+    private final Integer id;
+    private final Integer limit;
+
+    private Integer[] limitPool = {5000,10000,20000,30000};
+
+    public Card(Integer amount, ActorRef terminalActor) {
+        this.amount = amount;
+        this.terminalActor = terminalActor;
+
+        this.id = idCounter; // each Card will have unique ID
+        idCounter++;
+
+        Random rn = new Random(); // assign random limit
+        this.limit = limitPool[rn.nextInt()%4];
+    }
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .match(Transaction.class, x -> {
+                    //#Card-send-message
+                    terminalActor.tell(new receivedAmount(amount), getSelf());
+                    //#Card-send-message
+                })
+                .matchAny(o -> log.info("received unknown message"))
+                .build();
+    }
+//#Card-messages
+}
+//#Card-messages
---
+++ src/main/java/com/lightbend/akka/sample/Terminal.java
@@ -0,0 +1,43 @@
+package com.lightbend.akka.sample;
+
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.Props;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+
+//#Cart-messages
+public class Terminal extends AbstractActor {
+    private final LoggingAdapter log = Logging.getLogger(getContext().getSystem(), this);
+
+    //#Cart-messages
+    static public Props props() {
+        return Props.create(Terminal.class, () -> new Terminal());
+    }
+
+    //#printer-messages
+    static public class receivedAmount {
+        public final Integer amount;
+        //public final Integer cardID;
+
+        public receivedAmount(Integer amount){ //, Integer cardID) {
+            this.amount = amount;
+            //this.cardID = cardID;
+        }
+    }
+
+    public Terminal() {
+    }
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .match(receivedAmount.class, received -> {
+                    log.info("Amount : " + received.amount.toString()); // + "CardID" + received.cardID.toString());
+                })
+                .matchAny(o -> log.info("received unknown message"))
+                .build();
+    }
+//#Card-messages
+}
+//#Card-messages
