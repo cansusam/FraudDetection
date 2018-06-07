5692e2b5cab4125596a9ae09afeed91a876025d1
cansusam <cansusam@gmail.com>
2018-06-07 13:01:16 +0300

    Log info updated

--- src/main/java/com/lightbend/akka/sample/Terminal.java
+++ src/main/java/com/lightbend/akka/sample/Terminal.java
@@ -18,22 +18,27 @@ public class Terminal extends AbstractActor {
     //#printer-messages
     static public class receivedAmount {
         public final Integer amount;
-        //public final Integer cardID;
+        public final Integer cardID;
 
-        public receivedAmount(Integer amount){ //, Integer cardID) {
+        public receivedAmount(Integer amount, Integer cardID) {
             this.amount = amount;
-            //this.cardID = cardID;
+            this.cardID = cardID;
         }
     }
 
+    private static Integer idCounter = 0;
+    private final Integer id;
+
     public Terminal() {
+        this.id = idCounter;
+        idCounter++;
     }
 
     @Override
     public Receive createReceive() {
         return receiveBuilder()
                 .match(receivedAmount.class, received -> {
-                    log.info("Amount : " + received.amount.toString()); // + "CardID" + received.cardID.toString());
+                    log.info("Terminal " + id + " - Amount : " + received.amount.toString() + " - CardID : " + received.cardID.toString());
                 })
                 .matchAny(o -> log.info("received unknown message"))
                 .build();
