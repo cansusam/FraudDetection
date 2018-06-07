d03f471fbcb7aa6f44c8d8d46ca14abca0aab8c5
cansusam <cansusam@gmail.com>
2018-06-07 21:33:25 +0300

    Test with card_simple

--- src/main/java/com/lightbend/akka/sample/basicTransaction.java
+++ src/main/java/com/lightbend/akka/sample/basicTransaction.java
@@ -18,6 +18,7 @@ public class basicTransaction {
             final ActorRef cardActor = system.actorOf(Card.props(100,terminalActor));
             final ActorRef cardActor1 = system.actorOf(Card.props(200,terminalActor));
             final ActorRef cardActor2 = system.actorOf(Card.props(300,terminalActor));
+            final ActorRef cardActor3 = system.actorOf(Card_SimpleInit.props());
             //#create-actors
 
             //#main-send-messages
@@ -27,6 +28,7 @@ public class basicTransaction {
                 cardActor2.tell(new Card.Transaction(), ActorRef.noSender());
                 cardActor.tell(new Card.Transaction(), ActorRef.noSender());
                 cardActor2.tell(new Card.Transaction(), ActorRef.noSender());
+                cardActor3.tell(new Card_SimpleInit.Transaction(500,terminalActor),ActorRef.noSender());
 //            }while (System.in.available() == 0);
             //#main-send-messages
 
