43d578d831321fcc20d9e52d39e1e5bd9e13ee06
cansusam <cansusam@gmail.com>
2018-06-08 15:19:23 +0300

    Writer.java file added

---
+++ src/main/java/com/lightbend/akka/sample/Writer.java
@@ -0,0 +1,81 @@
+package com.lightbend.akka.sample;
+
+import akka.actor.AbstractActor;
+import akka.actor.Props;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.TimeZone;
+
+// TODO This class will be responsible of writing transactions to the .csv
+
+//#writerTransactions-messages
+public class Writer extends AbstractActor {
+    //#writerTransactions-messages
+    static public Props props() {
+        return Props.create(Writer.class, () -> new Writer());
+    }
+
+    private LoggingAdapter log = Logging.getLogger(getContext().getSystem(), this);
+
+    static private DateFormat df;
+    static private String date;
+    static private String fileName;
+
+    /**
+     * add new transaction line
+     */
+    static public class transactionAddLine{
+        public transactionAddLine(List<Integer> lineInfo) throws IOException {
+            String line = "";
+            line = lineInfo.get(0).toString();
+            for(int i=1; i<lineInfo.size(); i++){
+                line = line + ","  + lineInfo.get(i).toString();
+            }
+
+            BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true));
+            writer.append("\n"+line);
+            writer.close();
+        }
+    }
+
+    /**
+     * Create transaction log file .csv
+     */
+    static public class transactionFileCreate {
+        public transactionFileCreate() throws IOException {
+            // 1th file is transaction list
+            // CSV file headlines
+            String headlines = "CardID,TerminalID,Amount,Balance,Remaining,Validity,Date";
+            // File name with date/time
+            df = new SimpleDateFormat("yyyyMMdd-HHmmss");
+            df.setTimeZone(TimeZone.getTimeZone("GMT+3"));
+            date = df.format(new Date());
+            fileName = "transactions-" + date + ".csv";
+            BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
+            writer.write(headlines);
+            writer.close();
+        }
+    }
+
+    public Writer() {
+    }
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .match(transactionFileCreate.class, create -> {
+                    log.info("file created");
+                })
+                .build();
+    }
+//#writerTransactions-messages
+}
+//#writerTransactions-messages
